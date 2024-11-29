e3360298cc25bbf989861ef77fd7012ab3e913af
Ida Starek <ida.starek@gmail.com>
2024-11-29 12:48:33 +0000

    conntected DB

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+
+const appSettings = {
+    databaseURL: "https://shopping-list-app-54ea3-default-rtdb.europe-west1.firebasedatabase.app/";
+}
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value;
     
-    console.log(inputValue)
+    console.log(inputValue);
 })
