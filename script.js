a3c4ba1d0e606b4c94b3e0de1850e016a42262ad
Ida Starek <ida.starek@gmail.com>
2024-11-29 13:05:11 +0000

    connected to firebase DB

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
 const appSettings = {
-    databaseURL: "https://shopping-list-app-54ea3-default-rtdb.europe-west1.firebasedatabase.app/";
+    databaseURL: "https://shopping-list-app-54ea3-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings);
+
+console.log(app);
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
